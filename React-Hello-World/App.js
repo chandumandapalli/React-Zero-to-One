62c319a8120401991a2a248efd2869b8b0193e6b
chandumandapalli <chandumandapalli1@gmail.com>
2025-03-07 06:37:24 +0530

    Add files via upload

--- React-Hello-World/App.js
+++ React-Hello-World/App.js
@@ -110,14 +110,115 @@ console.log(heading); // returns object that is converted to React.createElement
 //so both of thema re same .. this JSX code react.createElement() method internally by babel.. 
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //creates a root object
-root.render(heading); //renders the heading element to the root element.
+root.render(heading); //renders the heading element to the root element. 
 
 
 
+/* { <h1 id="heading" className="heading-class">Hello World from React   !</h1>;
+=> the above is not understood by the JS engine , as JS engine only understands Ecma Script Syntax
+=> then how does it understand the above code ? 
+=> the above code is converted to below code by babel before it reaches JS   and then understood by JS engine.  
+=> ( babel is a JS compiler this is managed by the parcel bundler. converts here JSX to React code internally ) 
+=> JSX => React.createElement() => ReactElement - JS Object => ReactDOM.render() => HTML Element 
+=> Babel ( JSX to React.createElement() ) => JS Engine ( ReactElement ) => ReactDOM.render() => HTML Element
+=> 
+
+
+React.createElement("h1", { id: "heading", className: "heading-class" }, "Hello World from React   !"); } */
+// so the above code is understood by the JS engine.        
 
 
 
+// React Component :
+// A React component is a reusable piece of code that returns a React element. OR JSX code. 
+// A React component can be a class component or a functional component.
+// A class component is a JavaScript class that extends React.Component class.
+// A functional component is a JavaScript function that returns a React element.
+// A React component can be created using JSX or React.createElement() method.
+// A React component can be rendered using ReactDOM.render() method.
+// A React component can be nested inside another React component.
+// A React component can be passed props and state.
+// A React component can be updated using props and state.
+// A React component can be unmounted using ReactDOM.unmountComponentAtNode() method.
+// A React component can be updated using ReactDOM.render() method.
+// A React component can be updated using ReactDOM.hydrate() method.
+// A React component can be updated using ReactDOM.createPortal() method.
+// 2 types of components in React : 1. Class Component  ( Old way ) 2. Functional Component ( used now )
 
+// Main difference between below Functional components is that
+// first one returns implicit return and second one returns explicit return. 
+// implicit return using Arrow functions are wrapped inside ( ......) and for explicit return we use { return ( ......) }
+// both are same but explicit return is more readable.
+
+// const Heading1Component = () => (
+//   <h1 id="heading" className="heading-class">Hello World from React  using functional Component  !</h1>
+// )
+
+// const Heading2Component = () => {
+//     return <h1 id="heading" className="heading-class">Hello World from React  using functional Component  !</h1>;
+// };
+
+
+// React Component using Functional Component : 
+
+// const HeadingComponent = () => {
+//     return <h1 id="heading" className="heading-class">Hello World from React  using functional Component  !</h1>;
+// };
+
+// // React Element
+// const headingElement = (
+//     <h1 id="heading" className="heading-class">Hello World from React using heading Element    !</h1>
+// );
+
+// root.render(headingElement); //renders the heading element to the root element.
+// root.render(<HeadingComponent />); //renders the HeadingComponent to the root element.  overrided the previous element.
+
+const Title = function (){
+    return (
+      <div className="title">
+        <h1> This is a title </h1>
+        <h1> This is a title </h1>;
+      </div>
+    );
+       
+};
+
+// Here the point is reactTitle is just a normal JS variable and it is taking a React Element as a value. 
+// so it can used inside string interpolation also as normal variable in JS.
+const reactEle = (
+    <h1>This is a React elemetn and reactEle is just a JS variable .. that can be rendered using {} </h1>
+);
+
+const HeadingComponent = () => {
+    // This functional component returns a React element. 
+    return (
+        <div>
+             {/* nested component  --- also called component composition, including one component into another
+             complete code of Title functional component will be replaced Here . */}
+            <Title />
+            <h1> This is a heading </h1>
+            <JSInsideJSX />
+        </div>
+    );
+};
+const number = 0 ;
+const name = "hello";
+const JSInsideJSX = () => (
+    // using this syntax  any JS expression  code can be written inside JSX
+    // { }  is used to write JS code inside JSX
+    // string interpolation is also possible inside JSX using { }
+    <div  className="js-inside-jsx">
+        <p> {number} {20+ 40}
+        {console.log("Hello World")}
+        </p>
+        <p> {name} </p>
+        {/* Here reactEle is a normal JS variable and it is taking a React Element as a value. */}
+        {reactEle}
+    </div>
+);
+
+root.render(<HeadingComponent/>); //renders the HeadingComponent to the root element. 
+// overrided the previous element.
 
 
 
